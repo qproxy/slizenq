8483bc61962c23f15559c42760f5d75288dcbf77
qproxy <131621015+qproxy@users.noreply.github.com>
2023-04-24 00:51:15 +0300

    Add files via upload

---
+++ Home/script.js
@@ -0,0 +1,20 @@
+
+window.addEventListener('load', function() {
+document.querySelector('#box').style.top = '0';
+document.querySelector('#box').style.left = '0';
+    });
+                    
+window.addEventListener('load', function() {
+document.querySelector('#box_button').style.top = '0';
+document.querySelector('#box_button').style.left = '0';
+    });
+                    
+document.addEventListener("DOMContentLoaded", function() {
+var block = document.querySelector(".main_box");
+block.classList.add("show");
+    });
+                    
+window.addEventListener('load', function() {
+document.querySelector('#boxbox').style.top = '0';
+document.querySelector('#boxbox').style.left = '0';
+    });
